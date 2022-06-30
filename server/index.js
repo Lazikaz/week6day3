31690fa3f8e4b18da9fdbf89627f14dac5edf2a5
Lazikaz <souflakey@gmail.com>
2022-06-30 15:14:05 -0600

    a

--- server/index.js
+++ server/index.js
@@ -3,6 +3,18 @@ const path = require("path")
 
 const app = express()
 
+var Rollbar = require('rollbar');
+var rollbar = new Rollbar({
+  accessToken: '572f10b1affa4fde9e87d17687032568',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    code_version: '1.0.0',
+  }
+});
+
+rollbar.log("helloworld")
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
